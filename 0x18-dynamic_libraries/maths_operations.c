b2b447c2229bff37ccd6c2977d8753ede0ce138a
TshilidziT <Tshilidzibytee@gmail.com>
2023-12-11 09:51:35 -0800

    dynamic

---
+++ 0x18-dynamic_libraries/maths_operations.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**add - adds numbers
+ * @a: integer
+ * @b: integer
+ * Return: sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**sub - subtracts numbers
+ * @a: integer
+ * @b: integer
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**mul - multiplies numbers
+ * @a: integer
+ * @b: integer
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**div - divides numbers
+ * @a: integer
+ * @b: integer
+ * Return: a / b
+ */
+int div (int a, int b)
+{
+	return (a / b );
+}
+/**mod - modula
+ * @a: integer
+ * @b: integer
+ * Return: a % b
+ */
+int mod (int a, int b)
+{
+	return (a % b);
+}
